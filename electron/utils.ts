11037d220a49041d9344160953b1173f33644270
Sarim Abbas <sarim.stays@gmail.com>
2020-07-15 00:30:15 -0400

    comment drag code

--- electron/utils.ts
+++ electron/utils.ts
@@ -1,7 +1,12 @@
 import { ipcMain } from "electron";
+import PATH from "path";
 import dirTree from "directory-tree";
 
 ipcMain.handle("getFileTree", async (event, startPath) => {
   const tree = dirTree(startPath);
   return tree;
+});
+
+ipcMain.handle("pathJoin", async (event, ...paths) => {
+  return PATH.join(...paths);
 });
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,6 +3,34 @@ import { ipcRenderer } from "electron";
 import { Tree } from "antd";
 const { DirectoryTree } = Tree;
 
+class Position {
+  pos: number[];
+  constructor(pos: string) {
+    this.pos = pos.split("-").map((p) => parseInt(p));
+    this.pos.shift();
+  }
+
+  popRight() {
+    const res = this.pos.pop();
+    if (res === undefined) {
+      return -1;
+    }
+    return res;
+  }
+
+  popLeft() {
+    const res = this.pos.shift();
+    if (res === undefined) {
+      return -1;
+    }
+    return res;
+  }
+
+  toString() {
+    return this.pos.join("-");
+  }
+}
+
 const Sidebar = () => {
   const [treeData, setTreeData] = useState([]);
   const [expandedKeys, setExpandedKeys] = useState([]);
@@ -52,7 +80,9 @@ const Sidebar = () => {
   };
 
   const onDragEnter = (info: any) => {
-    console.log(info);
+    // this is the node that is hovered over while dragging
+    console.log("on drag enter", info);
+
     // expandedKeys 需要受控时设置
     // this.setState({
     //   expandedKeys: info.expandedKeys,
@@ -60,14 +90,25 @@ const Sidebar = () => {
   };
 
   const onDrop = (info: any) => {
-    console.log(info);
-    const dropKey = info.node.props.eventKey;
-    const dragKey = info.dragNode.props.eventKey;
+    console.log("on drop", info);
 
-    const dropPos = info.node.props.pos.split("-");
+    // * these are the user assigned keys for each node
+    // * you gotta make sure these are unique for the drop to work
+    const dropKey = info.node.key; // the thing being dropped on
+    const dragKey = info.dragNode.key; // the thing being dragged
+
+    // * three options for dropPosition:
+    // * -1 => it's before the highlighted node
+    // *  0 => it's on the highlighted node
+    // *  1 => it's after the highlighted node
+    const dropPos = info.node.pos.split("-");
     const dropPosition =
       info.dropPosition - Number(dropPos[dropPos.length - 1]);
+    console.log("drop position", dropPosition);
 
+    // this is a function that traverses the tree starting at "data" node as root
+    // it does this until the key === data.key, at which point it applies a callback
+    // to that data node
     const loop = (data: any, key: any, callback: any) => {
       for (let i = 0; i < data.length; i++) {
         if (data[i].key === key) {
@@ -78,59 +119,121 @@ const Sidebar = () => {
         }
       }
     };
+
+    // * Make a copy of all the data
     const data = [...treeData];
 
-    // Find dragObject
+    // * Remove the drag node from the children array of its parent
+    // Find dragObject i.e. the thing being dragged
     let dragObj: any;
     loop(data, dragKey, (item: any, index: any, arr: any) => {
+      // remove the drag node from the children[] of its parent
       arr.splice(index, 1);
       dragObj = item;
     });
 
+    // * the dragged node could end up in three places
     if (!info.dropToGap) {
-      // Drop on the content
+      // * 1. dropped onto another node without expanding it
+      // find the node which is being dropped on
+      // in the callback, get that node, and push to its children
+      console.log("FIRST CASE");
       loop(data, dropKey, (item: any) => {
         item.children = item.children || [];
-        // where to insert
         item.children.push(dragObj);
       });
+      // ------------
     } else if (
-      (info.node.props.children || []).length > 0 && // Has children
-      info.node.props.expanded && // Is expanded
+      (info.node.children || []).length > 0 && // if node target has children
+      info.node.expanded && // Is expanded
       dropPosition === 1 // On the bottom gap
     ) {
+      // * 2. hover over a node, it expands, and you put dragged node right after it
+      console.log("SECOND CASE");
       loop(data, dropKey, (item: any) => {
         item.children = item.children || [];
-        // where to insert
-        item.children.unshift(dragObj);
+        item.children.unshift(dragObj); // insert at start of children
       });
+      // ------------
     } else {
-      let ar;
-      let i;
+      // * 3. all other cases where you put dragged node either inside another node and
+      // * get before/after any of its children, or you go before/after current level siblings
+      console.log("THIRD CASE");
+      let ar: any[] = [];
+      let i: number = 0;
+      // loop until you find the target node
+      // find its index in its parent's children
+      // also get the whole array of chidlren
       loop(data, dropKey, (item: any, index: any, arr: any) => {
         ar = arr;
         i = index;
       });
       if (dropPosition === -1) {
+        // insert before the target node
         ar.splice(i, 0, dragObj);
       } else {
+        // insert after the target node
         ar.splice(i + 1, 0, dragObj);
       }
     }
 
     setTreeData(data);
+
+    // getPathFromPosition(info.node.pos);
+
+    // the dragNode is the node being dragged
+    // the position of that node is the original position
+
+    // dragNodesKeys: the two user assigned keys in an array
+    // the first key is the node being dragged
+    // the second key is the node that is dropped onto
+
+    // dropPostition is not useful; it is just the relative position
+    // within that folder i.e the first position, second, third etc
+
+    // node is the target node, which was dropped onto
+
+    // to move stuff in the file system
+    // 1. if dragNode.pos.split("-").length == node.pos.split("-").length
+    //    return
+    // 2.
+  };
+
+  const getPathFromPosition = async (pos: string) => {
+    // build up the path to this position
+    const p: Position = new Position(pos);
+    let path = "";
+    const pop = p.popLeft();
+    let currNode: any = treeData[pop];
+    path = await ipcRenderer.invoke("pathJoin", path, currNode.title);
+    while (p.pos.length > 0) {
+      const pop: number = p.popLeft();
+      currNode = currNode.children[pop];
+      path = await ipcRenderer.invoke("pathJoin", path, currNode.title);
+    }
+
+    // console.log(path);
+    // console.log(currNode);
+
+    console.log(currNode);
+
+    console.log(path);
+  };
+
+  const onSelect = (selectedKeys, other) => {
+    console.log("on click", selectedKeys, other);
   };
 
   return (
     <div>
       <h1>Hello, I'm a tree view</h1>
-      <DirectoryTree
+      <Tree
         treeData={treeData}
         onDragEnter={onDragEnter}
         onDrop={onDrop}
-        multiple
+        onSelect={onSelect}
+        autoExpandParent={false}
         draggable
-        blockNode
       />
     </div>
   );
